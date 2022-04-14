b65036c0eadedbce0dbde4a9cbec281d67226680
Aysel552 <a.abbasaliyeva1992@gmail.com>
2022-04-14 21:26:42 +0400

    solved an issue of remainder from two numbers

--- script/remainder_from_two_numbers.js
+++ script/remainder_from_two_numbers.js
@@ -1,3 +1,8 @@
 // Return the Remainder from Two Numbers
 // There is a single operator in JavaScript, capable of providing the remainder of a division operation. Two numbers are passed as parameters. The first parameter divided by the second parameter will have a remainder, possibly zero. Return that value.
 
+function remaninderFromTwoNumbers(x, y) {
+    return x % y
+}
+
+console.log(remaninderFromTwoNumbers(10, 3));
