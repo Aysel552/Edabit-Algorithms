1eeb784172d303c68a6916746c1fe6a892da91f8
Aysel552 <a.abbasaliyeva1992@gmail.com>
2022-04-14 21:12:54 +0400

    solved a task to convert minutes to seconds

---
+++ minutes_to_seconds.js
@@ -0,0 +1,7 @@
+// Write a function that takes an integer minutes and converts it to seconds.
+
+function convertMinutesToSeconds(minutes) {
+    return Math.floor(minutes * 60)
+}
+
+console.log(convertMinutesToSeconds(15));
